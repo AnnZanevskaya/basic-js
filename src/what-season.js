9068c32b5a382c9631bd92615ec024e3862f6cdc
Hanna <Hanna_Zaneuskaya@epam.com>
2020-03-08 19:21:41 +0300

    feat: implement what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,36 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+ module.exports = function getSeason(date) {
+   if (date == undefined) {
+     return "Unable to determine the time of year!";
+   }
+
+   if (date === undefined || !isValidDate(date)) {
+     throw new Error()
+   }
+
+   var month = date.getMonth();
+
+   switch (month) {
+     case 11:
+     case 0:
+     case 1:
+       return 'winter';
+     case 2:
+     case 3:
+     case 4:
+       return 'spring';
+     case 5:
+     case 6:
+     case 7:
+       return 'summer';
+     case 8:
+     case 9:
+     case 10:
+       return 'autumn';
+     default:
+       return false;
+   }
+ };
+
+ function isValidDate(date) {
+   return date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
+ }
