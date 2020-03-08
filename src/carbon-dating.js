a648ab56b48ec0c829669e708093dc142ec1ff87
Hanna <Hanna_Zaneuskaya@epam.com>
2020-03-08 18:32:19 +0300

    feat: implement carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,25 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!isString(sampleActivity) || isOutOfRange(sampleActivity) || !isNumberValue(sampleActivity)) {
+    return false;
+  }
+
+  let rate = Math.LN2.toFixed(3) / HALF_LIFE_PERIOD;
+  let years = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / rate;
+
+  return Math.ceil(years);
 };
+
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
+}
+
+function isNumberValue(value) {
+  return !(/[A-Za-z]/.test(value));
+}
+
+function isOutOfRange(value) {
+  return value <= 0 || value > MODERN_ACTIVITY
+}
