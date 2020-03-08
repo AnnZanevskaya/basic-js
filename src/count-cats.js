b1f0fa9162a0f63ea6b617cac0f15adea8a63cdd
Hanna <Hanna_Zaneuskaya@epam.com>
2020-03-08 18:07:59 +0300

    feat: implement count-cats funtion

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  const cat = "^^"
+  var matrixArray = [];
+
+  matrix.forEach(element => {
+    matrixArray = matrixArray.concat(element);
+  });
+
+  var catsCount = matrixArray.filter(f => f === cat).length
+
+  return catsCount;
 };
