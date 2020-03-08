e7ffa74f93953380aa55b6382bc1ef47baca72eb
Hanna <Hanna_Zaneuskaya@epam.com>
2020-03-08 18:49:31 +0300

    feat: implement dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+ module.exports = function createDreamTeam(members) {
+   if (!Array.isArray(members)) {
+     return false;
+   }
+
+   var filteredMembers = members.filter(f => isString(f));
+
+   var membersFirstLetters = filteredMembers.map(function (member) {
+     return member.replace(/ /g, '').toUpperCase().split("")[0];
+   });
+
+   return membersFirstLetters.sort().join("");
+ };
+
+ function isString(value) {
+   return typeof value === 'string' || value instanceof String;
+ }
