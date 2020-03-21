c8175b5d56174151ab75e8ccdf91d593a2002240
Hanna <Hanna_Zaneuskaya@epam.com>
2020-03-21 17:46:50 +0300

    feat: implement simple-chain functionality

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,46 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value === "") {
+      this.chain.push("( )");
+    } else {
+      this.chain.push(`( ${value} )`);
+    }
+
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position == '' || this.chain[position] === undefined) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let resultChain = '';
+
+    this.chain.map((element, i) => {
+      if (i == 0) {
+        resultChain += element;
+      } else {
+        resultChain += `~~${element}`;
+      }
+    });
+
+    this.chain = [];
+
+    return resultChain;
   }
 };
 
